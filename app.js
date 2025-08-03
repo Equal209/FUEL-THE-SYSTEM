9f0c7bb9b12df9a650f330ab27ad4fbe2fdf3f2f
Equal209 <dionjones902@gmail.com>
2025-08-03 12:42:26 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('#mobile-menu');
+const menulink = document.querySelector('.navbar__menu');
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menulink.classList.toggle('active');
+});
